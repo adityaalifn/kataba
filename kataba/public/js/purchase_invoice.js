b1cb26bcb39e5b1313b15f6f8bb5e031399b619a
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2019-01-09 14:01:24 +0000

    purchase invoice

--- kataba/public/js/purchase_invoice.js
+++ kataba/public/js/purchase_invoice.js
@@ -7,8 +7,28 @@ function setBilledAccount(frm) {
 		},
 		callback: function (data) {
 			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && frm.doc.supplier_name == data.message.company_name){
-				
+				console.log("A");
+				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_receive_account + "' where name='" + frm.doc.name + "' and voucher_type='Purchase Receipt'"
+				frappe.call({
+					"method": "kataba.client.run_sql",
+					args: {
+						"sql": sql
+					}
+				})
+				var sql2 = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_invoice_account + "' where name=frm.doc.name' and voucher_type='Purchase Invoice'" 
+				frappe.call({
+					"method": "kataba.client.run_sql",
+					args: {
+						"sql": sql
+					}
+				})
 			}
 		}
 	})
 }
+
+frappe.ui.form.on("Purchase Invoice", {
+	on_submit: function(frm) {
+		setBilledAccount(frm)
+	}
+})
