c2af2a2343886f7bfa6c5f7d858e527fe240d936
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2019-01-09 14:25:33 +0000

    purchase invoice

--- kataba/public/js/purchase_invoice.js
+++ kataba/public/js/purchase_invoice.js
@@ -6,6 +6,7 @@ function setBilledAccount(frm) {
 			"filters": {'company_name': frm.doc.company_name}
 		},
 		callback: function (data) {
+			console.log(data.message.company_name);
 			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && frm.doc.supplier_name == data.message.company_name){
 				console.log("A");
 				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_receive_account + "' where name='" + frm.doc.name + "' and voucher_type='Purchase Receipt'"
