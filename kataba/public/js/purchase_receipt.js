93e06695673131fdde9c11cd98d08c410808aa32
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2019-01-10 14:16:10 +0000

    Add purchase_receipt.js

---
+++ kataba/public/js/purchase_receipt.js
@@ -0,0 +1,29 @@
+function setBilledAccount(frm) {
+	frappe.call({
+		"method": "frappe.client.get",
+		args: {
+			"doctype": "Company",
+			"filters": {'name': frm.doc.supplier_name}
+		},
+		callback: function (data) {
+			console.log(data.message.company_name);
+			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && frm.doc.supplier_name == data.message.company_name){
+				console.log("A");
+				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_receive_account + "' where name='" + frm.doc.name + "' and voucher_type='Purchase Receipt'"
+				frappe.call({
+					"method": "kataba.client.run_sql",
+					args: {
+						"sql": sql
+					}
+				})
+			}
+		}
+	})
+}
+
+frappe.ui.form.on("Purchase Receipt", {
+	on_submit: function(frm) {
+		setBilledAccount(frm)
+	}
+})
+
