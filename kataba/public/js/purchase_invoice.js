aae6aa20bd69dc8468d3bcef594d3f12326c4315
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2019-01-10 13:17:58 +0000

    Update purchase_invoice.js

--- kataba/public/js/purchase_invoice.js
+++ kataba/public/js/purchase_invoice.js
@@ -9,18 +9,11 @@ function setBilledAccount(frm) {
 			console.log(data.message.company_name);
 			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && frm.doc.supplier_name == data.message.company_name){
 				console.log("A");
-				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_receive_account + "' where name='" + frm.doc.name + "' and voucher_type='Purchase Receipt'"
+				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_invoice_account + "' where name='" + frm.doc.name + "' and voucher_type='Purchase Invoice'" 
 				frappe.call({
 					"method": "kataba.client.run_sql",
 					args: {
 						"sql": sql
-					}
-				})
-				var sql2 = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_invoice_account + "' where name=frm.doc.name' and voucher_type='Purchase Invoice'" 
-				frappe.call({
-					"method": "kataba.client.run_sql",
-					args: {
-						"sql": sql2
 					}
 				})
 			}
