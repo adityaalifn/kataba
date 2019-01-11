569fd731042ad56675040c4b0dbf8bbe9895fe10
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2019-01-11 03:12:10 +0000

    Fix

--- kataba/public/js/purchase_invoice.js
+++ kataba/public/js/purchase_invoice.js
@@ -7,7 +7,7 @@ function setBilledAccount(frm) {
 		},
 		callback: function (data) {
 			console.log(data.message.umrah_stock_received_but_not_billed_invoice_account);
-			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && frm.doc.supplier_name == data.message.name){
+			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && isSupplierInChildTable(frm.doc.supplier, frm.doc.company)){
 				console.log("A");
 				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_invoice_account + "' where voucher_no='" + frm.doc.name + "'"
 				frappe.call({
@@ -17,6 +17,19 @@ function setBilledAccount(frm) {
 					}
 				})
 			}
+		}
+	})
+}
+
+function isSupplierInChildTable(supplier, parent) {
+	frappe.call({
+		"method": "frappe.client.get",
+		args: {
+			"doctype": "Supplier Link",
+			"filters": {'supplier': supplier, 'parent': parent}
+		},
+		callback: function (data) {
+			return true
 		}
 	})
 }
