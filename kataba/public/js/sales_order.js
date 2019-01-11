0cd2b6fe3a54f39d44628a1eb0e4d92ffb30750c
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2019-01-12 04:11:06 +0700

    Update sales_order.js

--- kataba/public/js/sales_order.js
+++ kataba/public/js/sales_order.js
@@ -1,3 +1,20 @@
+var isSaving = false, isLoaded = false, frm_copy;
+
+function formatMoney(n, c, d, t) {
+    var c = isNaN(c = Math.abs(c)) ? 2 : c,
+      d = d == undefined ? "," : d,
+      t = t == undefined ? "." : t,
+      s = n < 0 ? "-" : "",
+      i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+      j = (j = i.length) > 3 ? j % 3 : 0;
+  
+    return "Rp " + s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+function insertAfter(referenceNode, newNode) { //function to insert new HTML element after an existing element in document
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 function saveTotalCommission(frm) {
 	frappe.call({
 		"method": "frappe.client.get",
@@ -47,155 +64,143 @@ function saveTotalCommission(frm) {
 	})
 }
 
-function formatMoney(n, c, d, t) {
-  var c = isNaN(c = Math.abs(c)) ? 2 : c,
-    d = d == undefined ? "," : d,
-    t = t == undefined ? "." : t,
-    s = n < 0 ? "-" : "",
-    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
-    j = (j = i.length) > 3 ? j % 3 : 0;
+function loadCommissionData(frm) {    
+    // @desc: Hiding commission_rate and total_commission Input
+    if (!document.querySelector("[title='commission_rate'] .control-value-con")){
+        //Hide the real Commission Input to avoid validation on submit
+        document.querySelector("[title='commission_rate'] .control-value").style.display = "none";
+        document.querySelector("[title='total_commission'] .control-value").style.display = "none";
 
-  return "Rp " + s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-};
+        //Display Commission Input
+        document.querySelector("[title='commission_rate'] .control-value").outerHTML += `<div class="control-value-con like-disabled-input" style="">${data.message.commission_rate}</div>`; 
+        document.querySelector("[title='total_commission'] .control-value").outerHTML += `<div class="control-value-con like-disabled-input" style="">${formatMoney(umrahItemCount*data.message.commission_rate)}</div>`;
+    }
 
-function insertAfter(referenceNode, newNode) { //function to insert new HTML element after an existing element in document
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    // @desc: Duplicating Sales Partner Input
+    if (document.querySelector("input[data-fieldname='sales_partner']") === document.activeElement) {
+        //wait until data on ul element being loaded
+        if ($("[data-fieldname='sales_partner'] ul li").length > 0) { 
+            document.querySelector("input[data-fieldname='sales_partner']").style.display = "none";
+            var newSalesPartnerInput = document.createElement("input");
+            newSalesPartnerInput.className = "form-control sales-partner-con";
+            insertAfter(document.querySelector("input[data-fieldname='sales_partner']"), newSalesPartnerInput);
+            document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
+            document.querySelector(".sales-partner-con").focus();
+        }
+    }
+
+    // @desc: Reveal List of Sales Partner element and Link button when Sales Partner Input was clicked
+    if (document.querySelector(".sales-partner-con") === document.activeElement) {
+        // Show Sales Partner selection
+        document.querySelector("[data-fieldname='sales_partner'] ul").removeAttribute("hidden");
+        // Show Link button
+        document.querySelector("[data-fieldname='sales_partner'] .link-btn").style.display = "block";
+
+        document.querySelector("[data-fieldname='sales_partner'] ul").onclick = function() {
+            // Update Sales Partner Input value
+            document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
+            // Update Commission Rate Input value
+            document.querySelector("[title='commission_rate'] .control-value-con").value = document.querySelector("[title='commission_rate'] .control-value").value;
+
+            loadTotalCommission(frm, document.querySelector(".sales-partner-con").value);
+
+            // Empty the original Inputs
+            document.querySelector("input[data-fieldname='sales_partner']").value = "";
+            document.querySelector("[title='commission_rate'] .control-value").value = "";
+            // When user has selected sales partner, hide the selection
+            document.querySelector("[data-fieldname='sales_partner'] ul").setAttribute("hidden", true);
+            document.querySelector(".sales-partner-con").blur()
+        };
+    }else if (document.querySelector(".sales-partner-con") !== document.activeElement) {
+        document.querySelector("[data-fieldname='sales_partner'] ul").setAttribute("hidden", true);
+        document.querySelector("[data-fieldname='sales_partner'] .link-btn").style.display = "none";
+    }
 }
 
-function loadCommissionData(frm) {
+function loadTotalCommission(frm, partner_name) {
 	if (frm.doc.sales_partner !== "") {
 		frappe.call({
 			"method": "frappe.client.get",
 			args: {
 				"doctype": "Sales Partner",
-				"filters": {'partner_name': frm.doc.sales_partner}
+				"filters": {'partner_name': partner_name}
 			},
 			callback: function (data) {
 				if (!data.message.commission_type) {
-					frappe.msgprint("No commission type found in sales partner or sales partner doctype!")
-				}
-				if (!document.querySelector("[title='commission_rate'] .control-value-con")){
-					//Hide the real Commission Input to avoid validation on submit
-					document.querySelector("[title='commission_rate'] .control-value").style.display = "none";
-					document.querySelector("[title='total_commission'] .control-value").style.display = "none";
-// 					document.querySelector("input[data-fieldname='sales_partner']").style.display = "none";
+					frappe.msgprint("Warning: No commission type found in sales partner or sales partner type doctype!")
+                }
 
-					//Display Commission Input
-	// 				var newSalesPartnerInput = document.createElement("input");
-	// 				newSalesPartnerInput.className = "form-control sales-partner-con";
-	// 				insertAfter(document.querySelector("input[data-fieldname='sales_partner']"), newSalesPartnerInput);
-// 					document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
-					document.querySelector("[title='commission_rate'] .control-value").outerHTML += `<div class="control-value-con like-disabled-input" style="">${data.message.commission_rate}</div>`; 
-					document.querySelector("[title='total_commission'] .control-value").outerHTML += `<div class="control-value-con like-disabled-input" style="">${formatMoney(umrahItemCount*data.message.commission_rate)}</div>`;
-				}
-				if (document.querySelector("input[data-fieldname='sales_partner']") === document.activeElement) {
-					if ($("[data-fieldname='sales_partner'] ul li").length > 0) { //wait until data on ul element being loaded
-						document.querySelector("input[data-fieldname='sales_partner']").style.display = "none";
-						var newSalesPartnerInput = document.createElement("input");
-						newSalesPartnerInput.className = "form-control sales-partner-con";
-						insertAfter(document.querySelector("input[data-fieldname='sales_partner']"), newSalesPartnerInput);
-						document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
-						document.querySelector(".sales-partner-con").focus();
-					}
-				}
-				// If fake Sales Partner input is being selected
-				if (document.querySelector(".sales-partner-con") === document.activeElement) {
-					// Show Sales Partner selection
-					document.querySelector("[data-fieldname='sales_partner'] ul").removeAttribute("hidden");
-					document.querySelector("[data-fieldname='sales_partner'] .link-btn").style.display = "block";
-
-					document.querySelector("[data-fieldname='sales_partner'] ul").onclick = function() {
-						// Update Sales Partner Input value
-						document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
-						document.querySelector("input[data-fieldname='sales_partner']").value = "";
-						// When user selected sales partner, hide the selection
-						document.querySelector("[data-fieldname='sales_partner'] ul").setAttribute("hidden", true);
-						document.querySelector(".sales-partner-con").blur()
-					};
-				}else if (document.querySelector(".sales-partner-con") !== document.activeElement) {
-					document.querySelector("[data-fieldname='sales_partner'] ul").setAttribute("hidden", true);
-					document.querySelector("[data-fieldname='sales_partner'] .link-btn").style.display = "none";
-				}
+                var umrahItemCount = 0;
+                var amount = 0;
+                for (var i=0; i < cur_frm.doc.items.length; i++) {
+                    //console.log(cur_frm.doc.items[i])
+                    if (cur_frm.doc.items[i].item_group === "Umrah") {
+                        umrahItemCount += cur_frm.doc.items[i].qty;
+                        amount+=cur_frm.doc.items[i].amount;
+                    }
+                }
 
 				if (data.message.commission_type == "Value") {
-					var umrahItemCount = 0;
-					for (var i=0; i < cur_frm.doc.items.length; i++) {
-						//console.log(cur_frm.doc.items[i])
-						if (cur_frm.doc.items[i].item_group === "Umrah") {
-							umrahItemCount += cur_frm.doc.items[i].qty
-						}
-					}
-	// 				console.log("umrahItemCount",umrahItemCount)
-
-					// Update fake textbox values
-					document.querySelector("[title='commission_rate'] .control-value-con").innerHTML = data.message.commission_rate;
-					document.querySelector("[title='total_commission'] .control-value-con").innerHTML = formatMoney(umrahItemCount*data.message.commission_rate);
-	// 				console.log("hasil perhitungan:", umrahItemCount*data.message.commission_rate)
-	// 				console.log("QTY", umrahItemCount)
+					document.querySelector("[title='total_commission'] .control-value-con").value = formatMoney(umrahItemCount*data.message.commission_rate);
+                    //console.log("hasil perhitungan:", umrahItemCount*data.message.commission_rate)
+                    //console.log("QTY", umrahItemCount)
 				}else if (data.message.commission_type == "Percentage") {
-					var amount = 0;
-					for (var i=0; i < cur_frm.doc.items.length; i++) {
-						//console.log(cur_frm.doc.items[i])
-						if (cur_frm.doc.items[i].item_group === "Umrah") {
-							amount+=cur_frm.doc.items[i].amount
-						}
-					}
-	// 				console.log("Percentage", formatMoney(amount*(data.message.commission_rate/100)))
-					document.querySelector("[title='commission_rate'] .control-value-con").innerHTML = data.message.commission_rate;
-					document.querySelector("[title='total_commission'] .control-value-con").innerHTML = formatMoney(amount*(data.message.commission_rate/100));
+					document.querySelector("[title='total_commission'] .control-value-con").value = formatMoney(amount*(data.message.commission_rate/100));
 				}
 			}
 		})
 	}
 }
 
-var isSaving = false, isLoaded = false, frm_copy;
-
 frappe.ui.form.on("Sales Order", {
 	onload: function(frm) {
-		frm_copy = frm;
 		if (document.querySelector(`body[data-route='Form/Sales Order/${frm.docname}']`)){
+            frm_copy = frm;
 			isLoaded = true;
-			console.log("Kataba script loaded.")
+			console.log("Sales Order Section.");
 		}
 	},
 	sales_partner: function(frm) {
-		loadCommissionData(frm)
+		loadTotalCommission(frm, document.querySelector(".sales-partner-con").value);
 	},	
 	items: function(frm) {
-		loadCommissionData(frm)
+		loadTotalCommission(frm, document.querySelector(".sales-partner-con").value);
 	},	
 	validate: function(frm) {
 		isSaving = true
-	},
+    },
+    before_submit: function(frm) {
+        frappe.model.set_value(frm.doctype, frm.docname, "sales_partner", "Sales Partner #1");
+    }
 // 	after_insert: function(frm) {
 // 		console.log("After Insert");
 // 	}
-})
+});
+
 setInterval(function(){ 
 	if (isLoaded && frm_copy.doc.sales_partner !== ""){
 		loadCommissionData(frm_copy);
-	}
+    }
+
 	if (isSaving && isLoaded && frm_copy.doc.sales_partner !== "") {
 		if (document.querySelector(".btn.btn-primary.btn-sm.primary-action").innerText === "Save"){
 			//console.log("Waiting erpnext")
 		}
 		if (document.querySelector(".btn.btn-primary.btn-sm.primary-action").innerText === "Submit"){
-			console.log("Updating value")
-			saveTotalCommission(frm_copy)
-			isSaving=false
-			loadCommissionData(frm_copy)
+			console.log("Updating value");
+			saveTotalCommission(frm_copy);
+			isSaving=false;
+			loadCommissionData(frm_copy);
 		}
-	}
+    }
+
 	if (isLoaded && document.querySelector('.modal.fade.in')) {
 		// Hide a modal that said "Commission Rate cannot be greater than 100"
 		if (document.querySelector('.modal.fade.in .modal-body .msgprint')){
 			if (document.querySelector('.modal.fade.in .modal-body .msgprint').innerText.includes("Commission Rate cannot be greater than 100")) {
-				console.log("From erpnext: ", document.querySelector('.modal.fade.in .modal-body .msgprint').innerText)
-				document.querySelector('.modal.fade.in .btn-modal-close').click()
-				if (frm_copy.doc.sales_partner !== ""){
-					loadCommissionData(frm_copy);
-				}
+                console.log("From erpnext: ", document.querySelector('.modal.fade.in .modal-body .msgprint').innerText);
+
+                document.querySelector('.modal.fade.in .btn-modal-close').click();
 			}		
 		}
 	}
