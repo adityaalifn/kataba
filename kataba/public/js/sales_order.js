716a41164805707f8cbe7bf3be9dc5b61198fdc8
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2019-01-12 01:29:43 +0700

    Update sales_order.js

--- kataba/public/js/sales_order.js
+++ kataba/public/js/sales_order.js
@@ -97,7 +97,7 @@ function loadCommissionData(frm) {
 					document.querySelector("[title='total_commission'] .control-value").outerHTML += `<div class="control-value-con like-disabled-input" style="">${formatMoney(umrahItemCount*data.message.commission_rate)}</div>`;
 				}else {
 					// Update fake textbox values
-					document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
+// 					document.querySelector(".sales-partner-con").value = document.querySelector("input[data-fieldname='sales_partner']").value;
 					document.querySelector("[title='commission_rate'] .control-value-con").innerHTML = data.message.commission_rate;
 					document.querySelector("[title='total_commission'] .control-value-con").innerHTML = formatMoney(umrahItemCount*data.message.commission_rate);
 				}
