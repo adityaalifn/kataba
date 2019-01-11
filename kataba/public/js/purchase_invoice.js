4ec463521b4dc6652f2e67430c379574cf658836
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2019-01-11 23:32:55 +0700

    Update purchase_invoice.js

--- kataba/public/js/purchase_invoice.js
+++ kataba/public/js/purchase_invoice.js
@@ -9,7 +9,7 @@ function setBilledAccount(frm) {
 			console.log(data.message.supplier_link)
 			console.log(data.message.umrah_stock_received_but_not_billed_invoice_account);
 			if (frm.doc.is_pnr == 1 && frm.doc.update_stock == 1 && isSupplierInChildTable(frm.doc.supplier_name, frm.doc.company)){
-				console.log(isSupplierInChildTable(frm.doc.supplier_name, frm.doc.company);
+				console.log(isSupplierInChildTable(frm.doc.supplier_name, frm.doc.company));
 				var sql = "update `tabGL Entry` set account='" + data.message.umrah_stock_received_but_not_billed_invoice_account + "' where voucher_no='" + frm.doc.name + "'"
 				frappe.call({
 					"method": "kataba.client.run_sql",
