8d42fd7ccbd7ca9148be3153fc7da7cf68d7e9f4
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2019-01-14 17:24:24 +0700

    Update delivery_note.js

--- kataba/public/js/delivery_note.js
+++ kataba/public/js/delivery_note.js
@@ -1,4 +1,4 @@
-function setCommissionData(frm) {
+function getCompanyInfo(frm) {
     frappe.call({
 		"method": "frappe.client.get",
 		args: {
@@ -10,54 +10,81 @@ function setCommissionData(frm) {
             
             // Fetch umrah_item_group value
             item_group = data.message.umrah_item_group;
-
-            frappe.call({
-                "method": "frappe.client.get",
-                args: {
-                    "doctype": "Sales Partner",
-                    "filters": {'partner_name': frm.doc.sales_partner}
-                },
-                callback: function (data) {
-                    // frappe.model.set_value(doctype, name, fieldname, value)
-                    // Set value to mgs_commission_rate field
-                    frappe.model.set_value(frm.doctype, frm.docname, "mgs_commission_rate",data.message.mgs_commission_rate)
-                    
-                    // Counting parameters
-                    var total_commission = 0;
-                    var umrahItemCount = 0;
-                    var amount = 0;
-                    for (var i=0; i < cur_frm.doc.items.length; i++) {
-                        if (cur_frm.doc.items[i].item_group === item_group) {
-                            umrahItemCount += cur_frm.doc.items[i].qty;
-                            amount+=cur_frm.doc.items[i].amount;
-                        }
-                    }
-        
-                    if (data.message.mgs_commission_type === "Value") {
-                        total_commission = umrahItemCount*data.message.mgs_commission_rate
-                    }else if (data.message.mgs_commission_type === "Percentage") {
-                        total_commission = amount*(data.message.mgs_commission_rate/100)
-                    }
-                    
-                    frappe.model.set_value(frm.doctype, frm.docname, "total_commission", total_commission);
-                }
-            })
+            
+            setCommissionData(frm, item_group);
         }
     })
 }
 
+function setCommissionData(frm, item_group) {
+    frappe.call({
+        "method": "frappe.client.get",
+        args: {
+            "doctype": "Sales Partner",
+            "filters": {'partner_name': frm.doc.sales_partner}
+        },
+        callback: function (data) {
+            // frappe.model.set_value(doctype, name, fieldname, value)
+            
+            // Counting parameters
+            var total_commission = 0;
+            var umrahItemCount = 0;
+            var amount = 0;
+            for (var i=0; i < cur_frm.doc.items.length; i++) {
+                if (cur_frm.doc.items[i].item_group === item_group) {
+                    umrahItemCount += cur_frm.doc.items[i].qty;
+                    amount+=cur_frm.doc.items[i].amount;
+                }
+            }
+
+            if (data.message.mgs_commission_type === "Value") {
+                total_commission = umrahItemCount*data.message.mgs_commission_rate
+            }else if (data.message.mgs_commission_type === "Percentage") {
+                total_commission = amount*(data.message.mgs_commission_rate/100)
+            }
+            
+            if (frm.doc.status === "Draft" || frm.doc.status === "Completed" || frm.doc.status === "Cancelled" || frm.doc.status === "Closed") {
+                // Set value to mgs_commission_rate field
+                frappe.model.set_value(frm.doctype, frm.docname, "mgs_commission_rate",data.message.mgs_commission_rate)
+                frappe.model.set_value(frm.doctype, frm.docname, "total_commission", total_commission);
+            }else{
+                overrideTotalCommission(frm, total_commission);
+            }
+        }
+    })
+}
+
+function overrideTotalCommission(frm, total_commission) {
+    frappe.call({
+        "method": "kataba.client.run_sql",
+        args: {
+            "sql": "update `tabSales Order` set total_commission = " + total_commission + " where name = '" + frm.docname +"'"
+        }
+    })
+    
+    isSaving = false;
+}
+
+var isSaving = false;
+
 frappe.ui.form.on("Delivery Note", {
     onload: function(frm) {
         if (frm.doc.sales_partner !== "" && frm.doc.status === "Draft") {
-            setCommissionData(frm);
+            getCompanyInfo(frm);
         }
     },
     sales_partner: function(frm) {
-        setCommissionData(frm);
+        getCompanyInfo(frm);
     },
     items: function(frm) {
         if (frm.doc.sales_partner !== "") {
-            setCommissionData(frm);
+            getCompanyInfo(frm);
+        }
+    },
+    on_submit: function(frm) {
+        if (frm.doc.sales_partner !== "") {
+            isSaving = true;
+            getCompanyInfo(frm);
         }
     }
 })
